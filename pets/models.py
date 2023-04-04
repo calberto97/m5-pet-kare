9d5106e9d7d3e71c36b3849e543d0b100e828966
Caio Alberto <caioalberto-@hotmail.com>
2023-04-04 16:04:23 -0300

    feat: tarefa 2 finalizada

--- pets/models.py
+++ pets/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
 
-class Pet(models.Model):
-    class SexChoices(models.TextChoices):
-        MALE = "Male"
-        FEMALE = "Female"
-        NOT_INFORMED = "Not Informed"
+class SexChoices(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    NOT_INFORMED = "Not Informed"
 
+
+class Pet(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=1)
     sex = models.CharField(
-        max_length=20,
-        choices=SexChoices.choices,
-        default=SexChoices.NOT_INFORMED
+        max_length=20, choices=SexChoices.choices, default=SexChoices.NOT_INFORMED
     )
     group = models.ForeignKey(
         "groups.Group", on_delete=models.PROTECT, related_name="pets"
     )
-    ...
---
+++ pets/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+from .models import SexChoices
+
+
+class PetSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    weight = serializers.DecimalField(max_digits=5, decimal_places=1)
+    sex = serializers.ChoiceField(choices=SexChoices.choices, required=False)
+    group = GroupSerializer()
+    traits = TraitSerializer(many=True)
